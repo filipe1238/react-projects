dd9252291f9770b1a70e76665b233a70bcca29fe
filipe1238 <83041450+filipe1238@users.noreply.github.com>
2022-08-09 11:53:41 -0300

    finished color gen project

--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -2,13 +2,22 @@ import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
 const SingleColor = ({ color, hex, weight, type }) => {
+  const [alert, setAlert] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAlert(false)
+    }, 2000);
+  }, [alert])
   return <>
-    {type === 'tint' && <article className={`color `}
+
+    <article className={`color ${type === 'shade' ? (weight > 10) && 'color-light' : 'color-value'}`}
       style={{
         backgroundColor: `#${hex}`
       }}
       onClick={() => {
         navigator.clipboard.writeText('#' + hex)
+        setAlert(true)
       }}>
       <p className='color-value'>
         {weight}%
@@ -16,23 +25,9 @@ const SingleColor = ({ color, hex, weight, type }) => {
       <p className='color-value'>
         #{hex}
       </p>
+      {alert && <p className='alert'>Copied to clipboard</p>}
     </article>
-    }
-    {type === 'shade' && <article className={`color ${(weight > 10) && 'color-light'}`}
-      style={{
-        backgroundColor: `#${hex}`
-      }}
-      onClick={() => {
-        navigator.clipboard.writeText('#' + hex)
-      }}>
-      <p className='color-value'>
-        {weight}%
-      </p>
-      <p className='color-value'>
-        #{hex}
-      </p>
-    </article>
-    }
+
   </>
 
 }
